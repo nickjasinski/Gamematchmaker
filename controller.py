c0e963ab0e0c6118d0215b8d88dc5df9131fa6f9
Cbojan22 <73564465+Cbojan22@users.noreply.github.com>
2025-03-26 15:14:46 -0500

    Add files via upload

---
+++ controller.py
@@ -0,0 +1,6 @@
+
+class Controller:
+    def __init__(self, userid: int, gameid: int, profileid: int):
+        self.userid = userid
+        self.gameid = gameid
+        self.profileid = profileid
---
+++ database.py
@@ -0,0 +1,16 @@
+
+class Database:
+    def __init__(self, name: str):
+        self.name = name
+
+    def getUser():
+        pass
+
+    def getProfile():
+        pass
+
+    def getGame():
+        pass
+
+    def getWishlist():
+        pass
