0d673f609c4d86490eecc46167b30b0eb8f743de
josephschildt <schildtjoey@gmail.com>
2025-04-06 10:28:26 -0500

    added main menu

---
+++ main.py
@@ -0,0 +1,52 @@
+from controller import Controller
+
+def main_menu():
+    print("\n==== Video Game Finder ====")
+    print("1. Sign Up")
+    print("2. Log In")
+    print("3. Search for Game Title")
+    print("4. Save Game to Wishlist")
+    print("5. Write a Review")
+    print("6. Add Friend")
+    print("7. Manage Preferences")
+    print("0. Exit\n")
+    return input("Choose an option: ")
+    
+
+def run():
+    
+    current_user = None  # Placeholder for User 
+
+    while True:
+        choice = main_menu()
+
+        if choice == '1':
+            print("Signing up ...\n")
+
+        elif choice == '2':
+            print("Logging in ...\n")
+
+        elif choice == '3':
+            print("Searching for ...\n")
+
+        elif choice == '4':
+            print("Saving ... to wishlist ...\n")
+
+        elif choice == '5':
+            print("Writing a review ...\n")
+        
+        elif choice == '6':
+            print("Adding ... to friends list\n")
+
+        elif choice == '7':
+            print("Managing preferences ...\n")
+
+        elif choice == '0':
+            print("Goodbye! See you soon!\n")
+            break
+        
+        else:
+            print("Invalid choice. Please enter a number from 0 to 7.\n")
+
+if __name__ == "__main__":
+    run()
